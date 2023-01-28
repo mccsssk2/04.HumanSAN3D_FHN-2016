b9c052c4ab426e5d1b6c7019847921673d3711ac
Sanjay R. Kharche, Ph.D <srk.london@yahoo.com>
2023-01-28 12:52:43 -0500

    Delete 04.HumanSAN_FHN-2016 directory

--- 04.HumanSAN_FHN-2016/sk2D_2/copythat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p png/
-
-for i in `seq 1 24`
-do
-mkdir -p png/png_${i}
-cp dir_${i}/*.png png/png_${i}/
-done
--- 04.HumanSAN_FHN-2016/sk2D_2/runhSAN2D.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i = 0 ; i < 5000; i++ ))
-do
-	pvbatch --use-offscreen-rendering hSAN2D.py $i
-done
